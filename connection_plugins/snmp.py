c395a101d8f775f9682f3b9b4b5356687b6b7d53
Peter Nørlund <pch@ordbogen.com>
2015-11-24 22:30:23 +0100

    Major rewrite to use asyncore
    By using async I/O we can avoid foring a child process altogether

--- connection_plugins/snmp.py
+++ connection_plugins/snmp.py
@@ -19,23 +19,31 @@
 import subprocess
 import shutil
 import os
+import select
+import fcntl
 import json
 import syslog
 import traceback
 import base64
+import asyncore
+import time
 
 from ansible import utils, constants, errors
 from ansible.callbacks import vvv
+from pysnmp.carrier.asynsock import dispatch
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 from pysnmp.entity.rfc3413.oneliner import mibvar
+from pysnmp.entity import engine
 from pysnmp.proto import rfc1902
 from pysnmp.proto import rfc1905
+from pysnmp.carrier.asynsock.dgram import udp
 
 __all__ = ['Connection',
            'SnmpValue', 'OctetString', 'ObjectIdentifier', 'Integer32', 'Counter32', 'IpAddress', 'Gauge32', 'TimeTicks', 'Opaque', 'Counter64',
            'SnmpClient', 'SnmpError']
 
-snmp_connection_cache = dict()
+_cache = dict()
+_snmp_engine = None
 
 p = constants.load_config_file()
 SNMP_AUTH_PROTOCOL = constants.get_config(p, 'snmp', 'auth_protocol', 'SNMP_AUTH_PROTOCOL', 'none').lower()
@@ -53,7 +61,6 @@ class Connection(object):
         self.host = host
         self.port = port if port else 161
         self.has_pipelining = False
-
 
     def _get_snmp_auth(self):
         """ Get SNMP auth object """
@@ -101,43 +108,16 @@ class Connection(object):
                                   privProtocol=priv_protocol, privKey=priv_key,
                                   securityEngineId=SNMP_ENGINE_ID)
 
+    def _get_snmp_connection(self):
+        key = '%s:%d' % (self.host, self.port)
+        if key in _cache:
+            return _cache[key]
+
+        conn = _SnmpConnection(self.host, self.port, self._get_snmp_auth())
+        _cache[key] = conn
+        return conn
+
     def connect(self, port=None):
-        key = self.host + ':' + str(port if port else self.port)
-        if key not in snmp_connection_cache:
-            self.snmp_auth = self._get_snmp_auth()
-
-            pipe_to_server = os.pipe()
-            pipe_from_server = os.pipe()
-
-            pid = os.fork()
-            if pid == 0:
-                os.close(pipe_to_server[1])
-                os.close(pipe_from_server[0])
-                fd = os.open(os.devnull, os.O_RDWR)
-                if fd != 0:
-                    os.dup2(fd, 0)
-                if fd != 1:
-                    os.dup2(fd, 1)
-                if fd != 2:
-                    os.dup2(fd, 2)
-                if fd not in (0, 1, 2):
-                    os.close(fd)
-
-                os.setsid()
-                os.chdir('/')
-
-                server = _SnmpServer(pipe_to_server[0], pipe_from_server[1], self.host, self.port, self.snmp_auth)
-                server.run()
-                os._exit(0)
-            elif pid == -1:
-                raise errors.AnsibleError('Unable to fork')
-            else:
-                self.snmp_auth = None
-
-                snmp_connection_cache[key] = (pipe_from_server[0], pipe_to_server[1])
-
-        self.snmp_pipe_in = snmp_connection_cache[key][0]
-        self.snmp_pipe_out = snmp_connection_cache[key][1]
         return self
 
     def exec_command(self, cmd, tmp_path, become_user=None, sudoable=False, executable='/bin/sh', in_data=None):
@@ -150,12 +130,15 @@ class Connection(object):
             local_cmd = cmd
         executable = executable.split()[0] if executable else None
 
+        pipe_to_server = os.pipe()
+        pipe_from_server = os.pipe()
+
         # os.environ is special, so we copy it into a dictionary and modify the dictionary instead
         env = dict()
         for key in os.environ.keys():
             env[key] = os.environ[key]
-        env['SNMP_PIPE_IN'] = str(self.snmp_pipe_in)
-        env['SNMP_PIPE_OUT'] = str(self.snmp_pipe_out)
+        env['SNMP_PIPE_IN'] = str(pipe_from_server[0])
+        env['SNMP_PIPE_OUT'] = str(pipe_to_server[1])
 
         if 'PYTHONPATH' in env:
             env['PYTHONPATH'] = os.path.dirname(__file__) + ':' + env['PYTHONPATH']
@@ -171,8 +154,26 @@ class Connection(object):
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE,
                              env=env)
-        stdout, stderr = p.communicate()
-        return (p.returncode, '', stdout, stderr)
+
+        # pysnmp insist on using its own socket map, so we use that instead
+        conn = self._get_snmp_connection()
+        sock_map = conn.dispatcher.getSocketMap()
+        stdout = _BufferedDispatcher(asyncore.file_wrapper(p.stdout.fileno()), map=sock_map)
+        stderr = _BufferedDispatcher(asyncore.file_wrapper(p.stderr.fileno()), map=sock_map)
+        server = _Server(conn, pipe_to_server[0], pipe_from_server[1], map=sock_map)
+
+        while stdout.readable() or stderr.readable():
+            asyncore.poll(0.5, map=sock_map)
+            conn.dispatcher.handleTimerTick(time.time())
+
+        p.wait()
+
+        os.close(pipe_to_server[0])
+        os.close(pipe_to_server[1])
+        os.close(pipe_from_server[0])
+        os.close(pipe_from_server[1])
+        
+        return (p.returncode, '', stdout.data, stderr.data)
 
     def _transfer_file(self, in_path, out_path):
         """ transfer a file from local to local """
@@ -195,64 +196,122 @@ class Connection(object):
         self._transfer_file(in_path, out_path)
 
     def close(self):
+       pass
+
+class _SnmpConnection(object):
+    def __init__(self, host, port, auth):
+        self.dispatcher = dispatch.AsynsockDispatcher()
+        self.engine = engine.SnmpEngine()
+        self.engine.registerTransportDispatcher(self.dispatcher)
+        self.generator = cmdgen.AsynCommandGenerator(self.engine)
+        self.auth = auth
+        self.transport = cmdgen.UdpTransportTarget((host, port))
+
+    def get(self, object_ids, callback):
+        self.generator.getCmd(self.auth, self.transport, object_ids, callback)
+
+    def set(self, var_binds, callback):
+        self.generator.setCmd(self.auth, self.transport, var_binds, callback)
+
+class _BufferedDispatcher(asyncore.file_dispatcher):
+    def __init__(self, fd, map=None):
+        asyncore.file_dispatcher.__init__(self, fd, map)
+        self.data = ''
+        self._finished = False
+
+    def handle_expt(self):
         pass
 
-class SnmpError(Exception):
-    pass
+    def handle_read(self):
+        chunk = self.recv(1024)
+        if chunk:
+            self.data = self.data + chunk
+        else:
+            self._finished = True
 
-class SnmpValue(object):
-    def __str__(self):
-        return str(self.value)
+    def readable(self):
+        return not self._finished
 
-class OctetString(SnmpValue):
-    def __init__(self, value):
-        self.value = str(value)
+    def writable(self):
+        return False
 
-class ObjectIdentifier(SnmpValue):
-    def __init__(self, value):
-        subids = []
-        for subid in str(value).split('.'):
-            subids.append(str(int(subid)))
-        self.value = '.'.join(subids)
 
-class Integer32(SnmpValue):
-    pass
+class _ReceiveDispatcher(asyncore.file_dispatcher):
+    def __init__(self, fd, server, map=None):
+        asyncore.file_dispatcher.__init__(self, fd, map)
+        self._server = server
+        self._buffer = ''
+        self._finished = False
 
-class Counter32(SnmpValue):
-    pass
+    def readable(self):
+        return not self._finished
 
-class IpAddress(SnmpValue):
-    pass
+    def writable(self):
+        return False
 
-class Gauge32(SnmpValue):
-    pass
+    def handle_expt(self):
+        pass
 
-class TimeTicks(SnmpValue):
-    pass
+    def handle_read(self):
+        chunk = self.recv(1024)
+        if not chunk:
+            self._finished = True
+            return
 
-class Opaque(SnmpValue):
-    pass
+        self._buffer = self._buffer + chunk
+        while True:
+            pos = self._buffer.find('\n')
+            if pos == -1:
+                break
+            line = self._buffer[:pos]
+            pos = pos + 1
+            self._buffer = self._buffer[pos:]
 
-class Counter64(SnmpValue):
-    pass
+            self._server.handle_line(line)
 
-class _SnmpPeer(object):
-    def __init__(self, fd_in, fd_out):
-        self.stream_in = os.fdopen(fd_in, 'rU')
-        self.stream_out = os.fdopen(fd_out, 'w')
+class _TransmitDispatcher(asyncore.file_dispatcher):
+    def __init__(self, fd, map=None):
+        asyncore.file_dispatcher.__init__(self, fd, map)
+        self._buffer = ''
 
-    def _send(self, data):
-        line = '%s\n' % json.dumps(data, default=self._default_hook)
-        syslog.syslog('_send: %s' % line)
-        self.stream_out.write(line)
-        self.stream_out.flush()
+    def send_line(self, line):
+        self._buffer = self._buffer + line + '\n'
 
-    def _recv(self):
-        line = self.stream_in.readline()
-        syslog.syslog('_recv: %s' % line)
-        return json.loads(line, object_hook=self._object_hook)
+    def readable(self):
+        return False
+
+    def writable(self):
+        if self._buffer:
+            return True
+        else:
+            return False
+
+    def handle_expt(self):
+        pass
+
+    def handle_write(self):
+        cnt = self.send(self._buffer)
+        self._buffer = self._buffer[cnt:]
+        pass
+
+class _JsonRpcPeer(object):
+    def __init__(self):
+        pass
+
+    def transmit(self, json):
+        pass
+
+    def serialize(self, value):
+        return json.dumps(value, default=self._default_hook)
+
+    def unserialize(self, data):
+        return json.loads(data, object_hook=self._object_hook)
+
+    def send(self, **kwargs):
+        self.transmit(self.serialize(kwargs) + '\n')
 
     def _default_hook(self, o):
+        """ Convert object into JSON compatible objects """
         if isinstance(o, OctetString):
             return dict(__jsonclass__=['OctetString', base64.b64encode(o.value)])
         if isinstance(o, Opaque):
@@ -268,6 +327,7 @@ class _SnmpPeer(object):
         raise ValueError('Unsupported object type')
 
     def _object_hook(self, o):
+        """ Convert JSON data into objects """
         if not isinstance(o, dict):
             return o
         if '__jsonclass__' in o:
@@ -292,36 +352,31 @@ class _SnmpPeer(object):
             raise ValueError('Unsupported object type')
         return o
 
-class _SnmpServer(_SnmpPeer):
-    def __init__(self, fd_in, fd_out, host, port, auth):
-        super(_SnmpServer, self).__init__(fd_in, fd_out)
-        self.auth = auth
-        self.transport = cmdgen.UdpTransportTarget((host, port))
-        self.generator = cmdgen.CommandGenerator()
+class _Server(_JsonRpcPeer):
+    def __init__(self, conn, pipe_in, pipe_out, map=None):
+        self._conn = conn
+        self._receiver = _ReceiveDispatcher(pipe_in, self, map)
+        self._transmitter = _TransmitDispatcher(pipe_out, map)
 
-    def run(self):
-        try:
-            while True:
-                request = self._recv()
+    def transmit(self, json):
+        self._transmitter.send(json)
 
-                method_name = request['method']
-                params = request['params']
+    def handle_line(self, line):
+        request = self.unserialize(line)
+        method = request['method']
+        params = request['params']
+        id = request['id']
+        
+        method_name = 'rpc_' + method
 
-                method = getattr(self, method_name)
-                try:
-                    result = method(*params)
-                    self._send(dict(jsonrpc='2.0', result=result, id=request['id']))
-                except Exception as e:
-                    trace = traceback.format_exc().splitlines()
-                    syslog.syslog(str(e))
-                    for line in trace:
-                        syslog.syslog(line)
-                    self._send(dict(jsonrpc='2.0', error=dict(code=0, message=str(e)), id=request['id']))
-        except Exception as e:
-            trace = traceback.format_exc().splitlines()
-            syslog.syslog(str(e))
-            for line in trace:
-                syslog.syslog(trace)
+        method = getattr(self, method_name)
+        method(id, *params)
+
+    def _send_result(self, id, result):
+        self.send(jsonrpc='2.0', result=result, id=id)
+
+    def _send_error(self, id, error):
+        self.send(jsonrpc='2.0', error=dict(code=0, message=error), id=id)
 
     def _to_pysnmp(self, value):
         """ Convert connection plugin object into pysnmp objects """
@@ -375,50 +430,94 @@ class _SnmpServer(_SnmpPeer):
             return None
         raise SnmpError('Invalid type: %s' % type(value).__name__)
 
-    def get(self, *object_ids):
-        """ Fetch SNMP variables """
-        var_names = []
+    def rpc_get(self, id, *object_ids):
+        pysnmp_var_names = []
         for object_id in object_ids:
-            var_names.append(rfc1902.ObjectName(str(object_id)))
-        error_indication, error_status, error_index, varbinds = self.generator.getCmd(self.auth, self.transport, *var_names)
+            pysnmp_var_names.append(rfc1902.ObjectName(str(object_id)))
+        self._conn.get(pysnmp_var_names, (self._on_rpc_get, id))
 
+    def _on_rpc_get(self, handle, error_indication, error_status, error_index, var_binds, id):
         if error_indication:
-            raise SnmpError(error_indication)
-        if error_status:
-            raise SnmpError(error_status.prettyPrint())
+            self._send_error(id, error_indication)
+        elif error_status:
+            self._send_error(id, error_status.prettyPrint())
+        else:
+            res = []
+            for var_bind in var_binds:
+                res.append(self._from_pysnmp(var_bind[1]))
+            self._send_result(id, res)
 
-        res = list()
-        for varbind in varbinds:
-            res.append(self._from_pysnmp(varbind[1]))
-
-        return res
-
-    def set(self, var_binds):
-        """ Set SNMP variables """
+    def rpc_set(self, id, var_binds):
         pysnmp_var_binds = []
         for object_id, value in var_binds.items():
             pysnmp_var_binds.append((rfc1902.ObjectName(str(object_id)), self._to_pysnmp(value)))
-        error_indication, error_status, error_index, varbinds = self.generator.setCmd(self.auth, self.transport, *pysnmp_var_binds)
+        self._conn.set(pysnmp_var_binds, (self._on_rpc_set, id))
 
+    def _on_rpc_set(self, handle, error_indication, error_status, error_index, var_binds, id):
         if error_indication:
-            raise SnmpError(error_indication)
-        if error_status:
-            raise SnmpError(error_status.prettyPrint())
+            self._send_error(id, error_indication)
+        elif error_status:
+            self._send_error(id, error_status.prettyPrint())
+        else:
+            self._send_result(id, None)
 
-    def walk(self, var_names):
-        """ Iterate SNMP variables """
-        # TODO
-        return None
+    def rpc_walk(self, id, var_names):
+        self._send_result(id, None)
 
-class SnmpClient(_SnmpPeer):
+class SnmpError(Exception):
+    pass
+
+class SnmpValue(object):
+    def __str__(self):
+        return str(self.value)
+
+class OctetString(SnmpValue):
+    def __init__(self, value):
+        self.value = str(value)
+
+class ObjectIdentifier(SnmpValue):
+    def __init__(self, value):
+        subids = []
+        for subid in str(value).split('.'):
+            subids.append(str(int(subid)))
+        self.value = '.'.join(subids)
+
+class Integer32(SnmpValue):
+    pass
+
+class Counter32(SnmpValue):
+    pass
+
+class IpAddress(SnmpValue):
+    pass
+
+class Gauge32(SnmpValue):
+    pass
+
+class TimeTicks(SnmpValue):
+    pass
+
+class Opaque(SnmpValue):
+    pass
+
+class Counter64(SnmpValue):
+    pass
+
+class SnmpClient(_JsonRpcPeer):
     """ SNMP API for the modules """
 
     def __init__(self):
-        super(SnmpClient, self).__init__(int(os.getenv('SNMP_PIPE_IN')), int(os.getenv('SNMP_PIPE_OUT')))
+        self._pipe_in = os.fdopen(int(os.getenv('SNMP_PIPE_IN')), 'rU')
+        self._pipe_out = os.fdopen(int(os.getenv('SNMP_PIPE_OUT')), 'w')
+
+    def transmit(self, json):
+        self._pipe_out.write(json)
+        self._pipe_out.flush()
 
     def _call(self, method, *params):
-        self._send(dict(jsonrpc='2.0', method=method, params=params, id=1))
-        result = self._recv()
+        self.send(jsonrpc='2.0', method=method, params=params, id=1)
+        line = self._pipe_in.readline()
+        result = self.unserialize(line)
 
         if 'error' in result:
             raise SnmpError(result['error']['message'])
